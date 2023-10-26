550167770a8b6a79320ff5da028617c7f27f1791
adeelx5 <149037061+adeelx5@users.noreply.github.com>
2023-10-26 07:31:09 +0000

    datatypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,5 +1,19 @@
+"use strict"; //it treats all JS code as new version
+
+//01_variables
+
 let firstName = "Adeel"
-lastName = "Ahmad"
+let lastName = "Ahmad"
 const myAge = 26
 
 console.table([firstName,lastName,myAge])
+
+/*  
+02_dataTypes 
+*/
+
+// alert (4+4) we cannot use this because it is nodejs, not inside a browser so it won't work
+
+console.log(typeof "Ahmad" );
+console.log(typeof lastName );
+
